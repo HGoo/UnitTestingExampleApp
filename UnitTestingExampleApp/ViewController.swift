dcf4fce6975024b8b9a9a307bb73956ccb29e9c7
Nikolay <halemhood@icloud.com>
2022-04-01 14:32:10 +0300

    Initial Commit

---
+++ UnitTestingExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTestingExampleApp
+//
+//  Created by Николай Петров on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
